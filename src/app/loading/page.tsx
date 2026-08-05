c8b123e91ef3e0346ce19b0652af17a130e4c1ef
HongDaeEui <aviate8@gmail.com>
2026-08-06 00:56:36 +0900

    fix: 구글 시트 데이터 전송을 위한 Content-Type 수정

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -52,7 +52,7 @@ export default function LoadingPage() {
           await fetch(url, {
             method: 'POST',
             mode: 'no-cors', // Ignore CORS response 
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'text/plain;charset=utf-8' },
             body: JSON.stringify({
               q0: data.track,
               q1: data.q1,
